9602750f78adc1a9965c870f5bbd1de7171d17dd
Robson da Silva <me@rsilva.net>
2018-12-04 23:28:19 -0200

    Day 1 code added

---
+++ Day1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt")
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            RunPart1(input);
+            RunPart2(input);
+            Console.Read();
+        }
+
+        public static void RunPart1(int[] input)
+        {
+            Console.WriteLine("Day 1 - Part 1");
+            Console.WriteLine("Result: " + input.Sum(x => x));
+            Console.WriteLine("End");
+        }
+
+        public static void RunPart2(int[] input)
+        {
+            var current = 0;
+            var repeated = false;
+            var results = new List<int>();
+
+            while (!repeated)
+            {
+                foreach (var item in input)
+                {
+                    current += item;
+
+                    if (results.Contains(current))
+                    {
+                        repeated = true;
+                        break;
+                    }
+
+                    results.Add(current);
+                }
+            }
+
+            Console.WriteLine("Day 1 - Part 2");
+            Console.WriteLine("Result: " + current);
+            Console.WriteLine("End");        
+        }
+    }
+}
