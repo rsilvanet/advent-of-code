45c7d1d71e0da7178a3fad5ba23013c6574e1ea3
Robson da Silva <me@rsilva.net>
2018-12-16 21:51:31 -0200

    Day 6 part 1 code finished

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -15,64 +15,81 @@ namespace Day6
                     var cordX = int.Parse(split[0].Trim());
                     var cordY = int.Parse(split[1].Trim());
 
-                    return (cordX, cordY);
-                });
+                    return new Point(cordX, cordY);
+                })
+                .ToArray();
 
             RunPart1(input);
-            //RunPart2(input);
         }
 
-        public void RunPart1((int x, int t)[] input)
+        public static void RunPart1(Point[] input)
         {
-            var maxX = input.Max(cord => cord.x);
-            var maxY = input.Max(cord => cord.x);
-            var matrix = new char[maxX, maxY];
+            var maxX = input.Max(point => point.X) + 1;
+            var maxY = input.Max(point => point.Y) + 1;
+            var matrix = new char?[maxX, maxY];
 
             for (int i = 0; i < input.Length; i++)
             {
-                var cord = input[i];
-                var code = i + 65;
+                var point = input[i];
+                var character = Convert.ToChar(i + 65);
 
-                matrix[cord.x, cord.y] = code;
+                point.Value = character;
+                matrix[point.X, point.Y] = character;
+                point.Size++;
             }
-            
-            for (int x = 0; x < length; x++)
+
+            for (int x = 0; x < maxX; x++)
             {
-                for (int y = 0; y < length; y++)
+                for (int y = 0; y < maxY; y++)
                 {
-                    if (matrix[x,y] != default(char))
+                    if (matrix[x, y] == null)
                     {
-                        continue;
-                    }
+                        var closerDistancePoint = input
+                            .GroupBy(p => p.CalculateDistance(x, y))
+                            .OrderBy(g => g.Key)
+                            .First();
 
-                    var distances = new int[input.Length];
-                    var minDistanceIndex = 0;
-                    var minDistanceValue = int.MaxValue;
-
-                    for (int i = 0; i < input.Length; i++)
-                    {
-                        var cord = input[i];
-                        var distX = Math.Abs(cordX - x);
-                        var distY = Math.Abs(cordY - y);
-
-                        distances[i] = distX + distY;
-
-                        if (distances[i] < minDistanceValue)
-                        {
-                            minDistanceIndex = i;
-                            minDistanceValue = distances[i];
-                        }
-
-                        if (distances.Count(dist => dist == minDistanceValue) > 1)
+                        if (closerDistancePoint.Count() > 1)
                         {
                             matrix[x, y] = '.';
                         }
-                        else 
+                        else
                         {
-                            matrix[x, y] = matrix[cordX, cordY]; 
+                            var point = closerDistancePoint.First();
+                            matrix[x, y] = point.Value;
+                            point.Size++;
+
+                            if (x == 0 || y == 0 || x == maxX || y == maxY)
+                            {
+                                point.Infinite = true;
+                            }
                         }
                     }
                 }
+            }
+
+            Console.WriteLine("Day 6 - Part 1");
+            Console.WriteLine("Result: " + input.Where(s => !s.Infinite).Max(s => s.Size));
+            Console.WriteLine("End");
+        }
+
+        public class Point
+        {
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+            public char? Value { get; set; }
+            public int Size { get; set; }
+            public bool Infinite { get; set; }
+
+            public int CalculateDistance(int x, int y)
+            {
+                return Math.Abs(X - x) + Math.Abs(Y - y);
             }
         }
     }
